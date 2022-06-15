899af0f037de2a39785adbf86089e4646ae0996f
Vladimir Korchevskiy <burg2007@yandex.ru>
2022-06-16 00:09:32 +0300

    init isEmpti

---
+++ src/utils/mydash/isEmpty.js
@@ -0,0 +1,17 @@
+/*
+isEmpty(null); // => true
+isEmpty(true); // => true
+isEmpty(1); // => true
+isEmpty([1, 2, 3]); // => false
+isEmpty({ 'a': 1 }); // => false
+isEmpty('123'); // => false
+isEmpty(123); // => true
+isEmpty(''); // => true
+isEmpty(0); // => true
+Map
+Set
+*/
+
+function isEmpty(value) {}
+
+export default isEmpty;
